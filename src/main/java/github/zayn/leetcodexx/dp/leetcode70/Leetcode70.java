93714a1675f70258d859037c0cf6d9eb337d3460
zhangjiayi07 <zhangjiayi07@kuaishou.com>
2023-03-06 15:47:33 +0800

    1

--- src/main/java/github/zayn/leetcodexx/dp/leetcode70/Leetcode70.java
+++ src/main/java/github/zayn/leetcodexx/dp/leetcode70/Leetcode70.java
@@ -1,9 +1,36 @@
 package github.zayn.leetcodexx.dp.leetcode70;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Leetcode70 {
 
     /**
+     * 散列表
+     */
+    private Map<Integer, Integer> hasSolvedList = new HashMap<>();
+
+    /**
      * leetcode 70 爬楼梯
+     * 假设你正在爬楼梯。需要 n阶你才能到达楼顶。
+     * <p>
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：n = 2
+     * 输出：2
+     * 解释：有两种方法可以爬到楼顶。
+     * 1. 1 阶 + 1 阶
+     * 2. 2 阶
+     * 示例 2：
+     * <p>
+     * 输入：n = 3
+     * 输出：3
+     * 解释：有三种方法可以爬到楼顶。
+     * 1. 1 阶 + 1 阶 + 1 阶
+     * 2. 1 阶 + 2 阶
+     * 3. 2 阶 + 1 阶
      *
      * @param n
      * @return
@@ -29,10 +56,32 @@ public class Leetcode70 {
         }
     }
 
+    /**
+     * 添加散列表
+     * 爬楼梯
+     * @param n
+     * @return
+     */
+    public int climbStairsV3(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (hasSolvedList.containsKey(n)) {
+            return hasSolvedList.get(n);
+        }
+        int res = climbStairsV3(n - 1) + climbStairsV3(n - 2);
+        hasSolvedList.put(n, res);
+        return res;
+    }
+
     public static void main(String[] args) {
         Leetcode70 leetcode70 = new Leetcode70();
         int num = leetcode70.climbStairsV2(10);
         int num2 = leetcode70.climbStairs(10);
+        int num3 = leetcode70.climbStairs(10);
         System.out.println(num);
     }
 
--- src/main/java/github/zayn/leetcodexx/sort/BubbleSort.java
+++ src/main/java/github/zayn/leetcodexx/sort/BubbleSort.java
@@ -3,6 +3,11 @@ package github.zayn.leetcodexx.sort;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = new int[]{5, 1, 3, 4, 8, 7, 6};
+        int[] ret = bubbleSortV2(arr);
+        System.out.println(ret);
+    }
+
+    private static int[] bubbleSort(int[] arr) {
         for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -10,7 +15,29 @@ public class BubbleSort {
                 }
             }
         }
-        System.out.println(arr);
+        return arr;
+    }
+
+    /**
+     * 可以提前退出的冒泡
+     * @param arr
+     * @return
+     */
+    private static int[] bubbleSortV2(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            //提前退出冒泡循环标志位
+            boolean flag = false;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                break;
+            }
+        }
+        return arr;
     }
 
     private static void swap(int[] arr, int i, int j) {
